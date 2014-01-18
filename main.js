d609a7d230425d5ba8961f680af556113eddb4e7
muselk <mustafae@live.ca>
2014-01-18 09:41:14 -0500

    v0.2
    Small update, including background

--- main.js
+++ main.js
@@ -19,6 +19,21 @@ function buyCursor(){
 	document.getElementById('cursorCost').innerHTML = nextCost;
 };
 
+var grandmas = 0;
+
+function buyGrandma(){
+	var grandmaCost = Math.floor(100 * Math.pow(1.1,grandmas));
+	if(cookies >= grandmaCost){
+		grandmas = grandmas + 1;
+		cookies = cookies - grandmaCost;
+		document.getElementById('grandmas').innerHTML = grandmas;
+		document.getElementById('cookies').innerHTML = cookies;
+	};
+	var nextgCost = Math.floor(100 * Math.pow(1.1,cursors));
+	document.getElementById('grandmaCost').innerHTML = nextgCost;
+};
+
 window.setInterval(function(){
 	cookieClick(cursors);
+	cookieClick(grandmas*5);
 }, 1000);
